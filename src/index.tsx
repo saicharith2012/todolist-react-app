568b58b637bbc1cb98d1e751f20999b6fd2fb21d
saicharith2012 <saicharithpalabindhela@gmail.com>
2024-08-02 02:20:51 +0530

    Bug fix: Inserting non-null assertion operator for root element

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
-const rootElement: HTMLElement | null = document.getElementById("root");
+const rootElement: HTMLElement | null = document.getElementById("root")!;
 
-if (rootElement !== null) {
-  const root = ReactDOM.createRoot(rootElement);
+const root = ReactDOM.createRoot(rootElement);
 
-  root.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  );
-}
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
